e3c960bf75dad69d51c86170877642bae32686d0
Julian ranieri <jumpingafterrain@gmail.com>
2022-03-26 00:10:40 -0700

    handlebars and fetch request to users

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require('express')
-
 const app = express()
+const { engine } = require('express-handlebars')
+
+app.engine('.hbs', engine({extname: '.hbs'}));
+app.set('view engine', '.hbs');
+app.set('views', './views')
+
+//Set JSON parser
 app.use(express.json())
+
+// Routes
 app.use('/api/users', require('./routes/api/users'))
 
-const PORT = 4000
+//Main Static Page
+app.get('/', (req, res) => {
+  res.render('main', {layout : 'index'})
+});
 
+
+//Run
+const PORT = 4000
 app.listen({port: PORT}, async () => {
   console.log(`Server Running at http://localhost:${PORT}`)
 })
